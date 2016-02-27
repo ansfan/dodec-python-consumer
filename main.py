2b446e0862f1d05539dbcdc423b59aedf412c038
Lilli Szafranski <lillialexis@gmail.com,lilli@daoofdev.com>
2016-02-26 21:49:31 -0800

    [STOIC-9] Got matplotlib to display a pentagon of LEDs!!!

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+
+import matplotlib.pyplot as plt
 import sys, getopt
 import numpy as np
 from LED import LED
@@ -7,16 +9,31 @@ def parse(infilePath, outfilePath, scale):
     with open(infilePath) as infile:
         lines = infile.readlines()
 
-    w, h = lines[1].split("\t")
+    w, h = lines[1].strip().split("\t")
     lls  = lines[3:len(lines)]
     leds = []
 
     for ll in lls:
-        x, y, rho, phi = ll.split("\t")
+        x, y, rho, phi = ll.strip().split("\t")
         leds.append(LED(x, y, rho, phi))
 
     for led in leds:
         print(led.to_string())
+
+
+    N = 50
+    x = np.random.rand(N)
+    y = np.random.rand(N)
+    x1 = [led.x for led in leds]
+    y1 = [led.y for led in leds]
+    colors = np.random.rand(N)
+    area = np.pi * (15 * np.random.rand(N))**2  # 0 to 15 point radiuses
+
+    plt.scatter(x1, y1)
+    # plt.scatter(x, y, s=area, c=colors, alpha=0.5)
+    plt.show()
+
+
 
     return
 
