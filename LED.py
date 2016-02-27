c68b47f754b3638122da2e6333b9cf02dc30e3e9
Lilli Szafranski <lillialexis@gmail.com,lilli@daoofdev.com>
2016-02-26 21:14:10 -0800

    [STOIC-8] Consumer is consuming LED coord file.

---
+++ LED.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+class LED(object):
+    x   = 0.0
+    y   = 0.0
+    rho = 0.0
+    phi = 0.0
+
+    # The class "constructor" - It's actually an initializer
+    def __init__(self, x, y, rho, phi):
+        self.x   = float(x)
+        self.y   = float(y)
+        self.rho = float(rho)
+        self.phi = float(phi)
+
+
+    def to_string(self):
+        return str(self.x) + "\t" + str(self.y) + "\t" + str(self.rho)+ "\t" + str(self.phi) + "\n"
+
