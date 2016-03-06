23c041a7fd3d8690ec8d3629e807df10079da0cf
Lilli Szafranski <lillialexis@gmail.com,lilli@daoofdev.com>
2016-03-05 17:10:55 -0800

    [STOIC-14] Deleted a bunch of commented-out code.

--- Raindrops.py
+++ Raindrops.py
@@ -49,26 +49,6 @@ ser = serial.Serial(
     dsrdtr=True
 )
 
-# mutex = Lock()
-# last_received = ''
-# def receiving(ser, colors):
-#     global last_received
-#     buffer = []
-#
-#     while True:
-#         while ser.inWaiting() > 0:
-#             # line = ser.readline()
-#             #
-#             # string = line.decode()
-#             #
-#             # hexStrArray = string.strip().split(" ")
-#
-#             mutex.acquire()
-#
-#             colors.colors = ser.read(NUM_LEDS * COLOR_BYTES)
-#
-#             mutex.release()
-
 
 
 def parse(infilePath, outfilePath, scale):
@@ -137,49 +117,18 @@ def main(argv):
 
 
 
-
 if __name__ == "__main__":
     main(sys.argv[1:])
-
-    # ser = serial.Serial(
-    #     port='/dev/ttyS1',
-    #     baudrate=9600,
-    #     stopbits=serial.STOPBITS_TWO,
-    #     bytesize=serial.EIGHTBITS,
-    #     rtscts=True,
-    #     dsrdtr=True
-    # )
-
-    # Thread(target=receiving, args=(ser, colors, )).start()
 
 
 
 # Create new Figure and an Axes which fills it.
-#fig = plt.figure(figsize=(window[0], window[1]))
-# ax = fig.add_axes([0, 0, window[0], window[0]], frameon=True)
+fig, ax = plt.subplots()
 
-fig, ax = plt.subplots()  #create figure and axes
-
-#fig(figsize=(window[0], window[1]))
 ax.set_aspect('equal')
-ax.set_xlim(0, window[0])# if (window[0] > window[1]) else window[1])
-ax.set_ylim(0, window[1])# if (window[0] > window[1]) else window[1])
-# ax = fig.add_axes([0, 0, 1, 1], frameon=True)
-# ax.set_xlim(0, 1), ax.set_xticks([])
-# ax.set_ylim(0, 1), ax.set_yticks([])
+ax.set_xlim(0, window[0])
+ax.set_ylim(0, window[1])
 
-
-# Create rain data
-n_drops = 50
-rain_drops = np.zeros(n_drops, dtype=[('position', float, 2),
-                                      ('size',     float, 1),
-                                      ('growth',   float, 1),
-                                      ('color',    float, 4)])
-
-# Initialize the raindrops in random positions and with
-# random growth rates.
-rain_drops['position'] = np.random.uniform(0, 1, (n_drops, 2))
-rain_drops['growth'] = np.random.uniform(50, 200, n_drops)
 
 x_data = [led.x for led in leds]
 y_data = [led.y for led in leds]
@@ -189,11 +138,7 @@ area   = [led.area for led in leds]
 # Construct the scatter which we will update during animation
 # as the raindrops develop.
 scat = ax.scatter(x_data, y_data,
-                  s=area, alpha=1.0)#, lw=0.5,
-                  #facecolors=colors)
-# scat = ax.scatter(rain_drops['position'][:, 0], rain_drops['position'][:, 1],
-#                   s=rain_drops['size'], lw=0.5, edgecolors=rain_drops['color'],
-#                   facecolors='none')
+                  s=area, alpha=1.0)
 
 printStrs = ["r: ", "g: ", "b: ", "a: "]
 
@@ -201,45 +146,7 @@ def update(frame_number):
     # Get an index which we can use to re-spawn the oldest raindrop.
     current_index = frame_number % len(leds)
 
-    # startingHue = frame_number % 255
-    # colors = []
-    #
-    # i = 0
-    # while (i < len(leds)):
-    #     hue = startingHue if startingHue < 255 else startingHue - 255
-    #     # r,g,b = cs.hsv_to_rgb(hue / 255)
-    #     # rgba = [r, g, b, 1.0]
-    #     colors.append(cs.hsv_to_rgb(hue / 255, 1.0, 1.0))
-    #     startingHue += 1
-    #     i += 1
-
     print("Frame: " + str(frame_number))
-
-    # # Make all colors more transparent as time progresses.
-    # rain_drops['color'][:, 3] -= 1.0/len(rain_drops)
-    # rain_drops['color'][:, 3] = np.clip(rain_drops['color'][:, 3], 0, 1)
-    #
-    # # Make all circles bigger.
-    # rain_drops['size'] += rain_drops['growth']
-    #
-    # # Pick a new position for oldest rain drop, resetting its size,
-    # # color and growth factor.
-    # rain_drops['position'][current_index] = np.random.uniform(0, 1, 2)
-    # rain_drops['size'][current_index] = 5
-    # rain_drops['color'][current_index] = (0, 0, 0, 1)
-    # rain_drops['growth'][current_index] = np.random.uniform(50, 200)
-    #
-    # # Update the scatter collection, with the new colors, sizes and positions.
-    # scat.set_edgecolors(rain_drops['color'])
-    # scat.set_sizes(rain_drops['size'])
-    # scat.set_offsets(rain_drops['position'])
-
-    # colors = np.random.rand(len(leds))
-
-    # mutex.acquire()
-
-    # while True:
-    inWaiting = ser.inWaiting
 
     if ser.inWaiting() > 0:
         print("We have stuff!!")
@@ -276,12 +183,7 @@ def update(frame_number):
     else:
         print("We don't have stuff. :-(")
 
-    #scat.set_facecolors(colors)
     scat.set_facecolors(globals.colors)
-
-    # mutex.release()
-
-
 
 
 
